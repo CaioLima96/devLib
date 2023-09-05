b674ebf759ac9e3b308f74e8fd227f16d4aa235d
CaioLima96 <caiovcastrolima@gmail.com>
2023-09-05 15:41:06 -0300

    chore: terminado o darkmode

--- src/components/Summary/index.jsx
+++ src/components/Summary/index.jsx
@@ -1,38 +1,26 @@
 import React from "react";
 
-import {SummaryDiv,} from './styles'
+import { SummaryDiv } from './styles'
 
 export default function Summary(props) {
 
-    // list={[{name:'', anchor: ''}]},
-
-    return(
+    return (
 
         <>
-
-            {/* <div id="summary">
+            <SummaryDiv className="summary">
 
                 <p>Summary</p>
 
                 <ul>
-                    <li><a href="#footer1">Simple Footer</a></li>
-                    <li><a href="#footer2">About/ContactUs Footer</a></li>
+                    {props.list.map((el, i) => {
+
+                        return (
+                            <li key={i}>
+                                <a href={`${el.anchor}`}>{`${el.name}`}</a>
+                            </li>
+                        )
+                    })}
                 </ul>
-
-            </div> */}
-
-            <SummaryDiv>
-
-                <p>Summary</p>
-
-                {props.list.map((el, i) => {
-
-                    return (
-                        <li key={i}>
-                            <a href={`${el.anchor}`}>{`${el.name}`}</a>
-                        </li>
-                    )
-                })}
 
             </SummaryDiv>
         </>
